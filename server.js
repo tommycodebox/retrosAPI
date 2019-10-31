393f3914f274492d3ba82f9f8b316ac5529b29cd
Tommy <tommycodebox@gmail.com>
2019-10-31 10:31:33 +0100

    Added socket server

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
+const cors = require('cors');
 const express = require('express');
 const app = express();
-const cors = require('cors');
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
 const connectDB = require('./db');
 require('dotenv').config();
 
@@ -18,9 +20,18 @@ app.use('/auth', require('./routes/auth'));
 app.use('/users', require('./routes/users'));
 app.use('/retros', require('./routes/retros'));
 
+// Sockets
+io.on('connection', socket => {
+  console.log('User connected');
+
+  socket.on('msg', msg => {
+    socket.broadcast.emit(msg.mob, msg);
+  });
+});
+
 // 404
 app.use((_, res) => res.status(404).json({ error: '404 | Page not found' }));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+server.listen(PORT, () => console.log(`Server started on port ${PORT}`));
